941d9768e16025c0b5124afb77ae6f83f2d659cc
eosun77 <eosun77@gmail.com>
2023-11-17 11:14:03 +0900

    🚧 1. find user by name

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,15 +4,19 @@ export async function POST(req: Request) {
   const data = await req.json();
   const name = data.name;
 
+  if (!name || typeof name !== "string" || name.trim().length === 0) {
+    return Response.json(
+      { message: "Invalid name provided." },
+      { status: 400 }
+    );
+  }
+
   try {
     const user = await prisma.user.findUnique({ where: { name } });
     if (!user)
-      return Response.json(
-        { message: "일치하는 사용자가 없습니다." },
-        { status: 404 }
-      );
+      return Response.json({ message: "User not found." }, { status: 404 });
     return Response.json({ data: user }, { status: 200 });
   } catch {
-    return Response.json({ message: "데이터베이스 오류" }, { status: 500 });
+    return Response.json({ message: "Database error." }, { status: 500 });
   }
 }
